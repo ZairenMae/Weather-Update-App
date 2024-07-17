83daa76b5daab66903313c52b7a6cec80630d59d
zai <zairenninofranco@gmail.com>
2024-07-17 18:51:54 +0800

    first commit

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import customtkinter
 from PIL import Image
 import datetime
-
-# YOUR open weather api key = YOUR API KEY
 from pyowm.owm import OWM
 from pyowm.utils.config import get_default_config
-configDict=get_default_config()
-configDict['language']='en'
-owmKey='YOUR API KEY'
+
+# Replace with your OpenWeather API key
+owmKey = 'pt5ymDL8lwGFLTjigjya5L19LlcNjt9E'
+configDict = get_default_config()
+configDict['language'] = 'en'
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("blue")
@@ -20,7 +20,7 @@ def getDateNow():
     month = int(datetime.datetime.now().month)
     day = int(datetime.datetime.now().day)
     year = int(datetime.datetime.now().year)
-    month_name = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+    month_name = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     return f"{month_name[month-1]} {day}, {year}"
 
 weatherData = {
@@ -31,54 +31,57 @@ weatherData = {
     'humidity': 0
 }
 
-frame=customtkinter.CTkLabel(app,text="")
-frame.grid(row=0,column=0,sticky="w",padx=50,pady=20)
+frame = customtkinter.CTkLabel(app, text="")
+frame.grid(row=0, column=0, sticky="w", padx=50, pady=20)
 
 def dynamiclabels():
-    cityLabel=customtkinter.CTkLabel(frame,text=weatherData['place'],font=('Arial BOLD', 25),anchor="w",width=180)
-    cityLabel.grid(row=0,column=0,sticky="w")
-    dateLabel=customtkinter.CTkLabel(frame,text=getDateNow(),font=('Arial', 15),anchor="e",width=200)
-    dateLabel.grid(row=0,column=1,sticky="e")
+    cityLabel = customtkinter.CTkLabel(frame, text=weatherData['place'], font=('Arial BOLD', 25), anchor="w", width=180)
+    cityLabel.grid(row=0, column=0, sticky="w")
+    dateLabel = customtkinter.CTkLabel(frame, text=getDateNow(), font=('Arial', 15), anchor="e", width=200)
+    dateLabel.grid(row=0, column=1, sticky="e")
     try:
-        weatherImg=customtkinter.CTkImage(Image.open(f"weatherImages/{weatherData['status']}.png"),size=(120,120))
+        weatherImg = customtkinter.CTkImage(Image.open(f"weatherImages/{weatherData['status']}.png"), size=(120, 120))
     except Exception as e:
-        weatherImg=customtkinter.CTkImage(Image.open(f"weatherImages/default.png"),size=(120,120))
-        print('image not found, default image will be selected.')
-    weatherImgLabel=customtkinter.CTkLabel(frame,text='',image=weatherImg,anchor="center")
-    weatherImgLabel.grid(row=1,column=0,columnspan=2,pady=20,sticky="nsew")
-    weatherLabel=customtkinter.CTkLabel(frame,text=weatherData['status'],font=('Arial BOLD',15),anchor="center")
-    weatherLabel.grid(row=2,column=0,columnspan=2,pady=[0,20],sticky="nsew")
-    tempLabel=customtkinter.CTkLabel(frame,text=f"Temp: {weatherData['temp']}°(celcius) ",font=('Arial',12),anchor="w")
-    tempLabel.grid(row=3,column=0,padx=2,pady=0,sticky="w")
-    humLabel=customtkinter.CTkLabel(frame,text=f"Humidity: {weatherData['humidity']}",font=('Arial',12),anchor="w")
-    humLabel.grid(row=4,column=0,padx=2,pady=0,sticky="w")
-    heatLabel=customtkinter.CTkLabel(frame,text=f"Heat Index: {weatherData['heatIndex']}",font=('Arial',12),anchor="w")
-    heatLabel.grid(row=5,column=0,padx=2,pady=0,sticky="w")
+        weatherImg = customtkinter.CTkImage(Image.open(f"weatherImages/default.png"), size=(120, 120))
+        print('Image not found, default image will be selected.')
+    weatherImgLabel = customtkinter.CTkLabel(frame, text='', image=weatherImg, anchor="center")
+    weatherImgLabel.grid(row=1, column=0, columnspan=2, pady=20, sticky="nsew")
+    weatherLabel = customtkinter.CTkLabel(frame, text=weatherData['status'], font=('Arial BOLD', 15), anchor="center")
+    weatherLabel.grid(row=2, column=0, columnspan=2, pady=[0, 20], sticky="nsew")
+    tempLabel = customtkinter.CTkLabel(frame, text=f"Temp: {weatherData['temp']}°C", font=('Arial', 12), anchor="w")
+    tempLabel.grid(row=3, column=0, padx=2, pady=0, sticky="w")
+    humLabel = customtkinter.CTkLabel(frame, text=f"Humidity: {weatherData['humidity']}%", font=('Arial', 12), anchor="w")
+    humLabel.grid(row=4, column=0, padx=2, pady=0, sticky="w")
+    heatLabel = customtkinter.CTkLabel(frame, text=f"Heat Index: {weatherData['heatIndex']}", font=('Arial', 12), anchor="w")
+    heatLabel.grid(row=5, column=0, padx=2, pady=0, sticky="w")
 
 dynamiclabels()
 
 def getweather(place):
-    owm=OWM(owmKey,configDict)
-    mgr=owm.weather_manager()
-    observation=mgr.weather_at_place(place)
-    w=observation.weather
-    weatherData['status']=w.detailed_status
-    weatherData['temp']=w.temperature('celsius')['temp_max']
-    weatherData['heatIndex']=w.heat_index
-    weatherData['humidity']=w.humidity
-    dynamiclabels()
-    print(weatherData)
+    owm = OWM(owmKey, configDict)
+    mgr = owm.weather_manager()
+    try:
+        observation = mgr.weather_at_place(place)
+        w = observation.weather
+        weatherData['status'] = w.detailed_status
+        weatherData['temp'] = w.temperature('celsius')['temp_max']
+        weatherData['heatIndex'] = w.heat_index
+        weatherData['humidity'] = w.humidity
+        dynamiclabels()
+        print(weatherData)
+    except Exception as e:
+        print(f"Error fetching weather data: {e}")
 
 def updateWeather():
-    weatherData['place']=str(placesCombo.get())
+    weatherData['place'] = str(placesCombo.get())
     getweather(weatherData['place'])
 
 getweather(weatherData['place'])
 
-placesCombo=customtkinter.CTkComboBox(frame,values=["Iloilo, PH","Manila, PH","Cebu, PH","Bacolod, PH"])
-placesCombo.grid(row=4,column=1,padx=2,pady=0,sticky="e")
-submitBtn=customtkinter.CTkButton(frame,text="Get Weather",width=50,command=updateWeather)
-submitBtn.grid(row=5,column=1,padx=1,pady=5,ipady=0,sticky="e")
+placesCombo = customtkinter.CTkComboBox(frame, values=["Iloilo, PH", "Manila, PH", "Cebu, PH", "Bacolod, PH"])
+placesCombo.grid(row=4, column=1, padx=2, pady=0, sticky="e")
+submitBtn = customtkinter.CTkButton(frame, text="Get Weather", width=50, command=updateWeather)
+submitBtn.grid(row=5, column=1, padx=1, pady=5, ipady=0, sticky="e")
 
 app.resizable(False, False) 
 app.mainloop()
